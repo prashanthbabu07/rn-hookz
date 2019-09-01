47c1b1db696135e16a164cf28c832e1e30ab545b
Prashanth Babu T <prashanthbabu07@icloud.com>
2019-09-01 16:28:19 +0530

    refactoring

--- ios-android/App.js
+++ ios-android/App.js
@@ -4,7 +4,7 @@
  */
 
 import React, { Component } from 'react';
-import Counter from './components/Counter';
+import Counter from './components/counter/Counter';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 import { INCREMENT_COUNTER, DECREMENT_COUNTER, TOGGLE } from './reducers/actions/Counter';
--- ios-android/components/counter/Counter.js
+++ ios-android/components/counter/Counter.js
@@ -16,8 +16,8 @@ import
     TouchableOpacity
 } from 'react-native';
 import { useSelector, useDispatch } from 'react-redux';
-import { increment, decrement, incrementAsync } from '../reducers/actions/Counter';
-import { toggle } from '../reducers/actions/Signed';
+import { increment, decrement, incrementAsync } from '../../reducers/actions/Counter';
+import { toggle } from '../../reducers/actions/Signed';
 
 const styles = StyleSheet.create({
     container: {
