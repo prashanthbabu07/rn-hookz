c9b6237e4ecb159020e90e260bf49227068ec96e
Prashanth Babu T <prashanthbabu07@gmail.com>
2019-09-16 23:08:44 +0530

    Added keyboard aware scrollview

--- app/views/onboarding/OnboardingRegistration.js
+++ app/views/onboarding/OnboardingRegistration.js
@@ -3,7 +3,7 @@
  * @flow
  */
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import
 {
     View,
@@ -11,7 +11,8 @@ import
     StyleSheet,
     TouchableOpacity,
     TextInput,
-    KeyboardAvoidingView
+    KeyboardAvoidingView,
+    Keyboard
 } from "react-native";
 import { Button } from "react-native-elements";
 import userDefault from "../../services/sqlite/defaults/UserDefault";
@@ -19,6 +20,7 @@ import { USER_ONBOARDED } from "../../services/sqlite/defaults/keys";
 import { APP_ROUTE_NAMES } from "../../constants/RouteNames";
 import AsyncStorage from "@react-native-community/async-storage";
 import { KV_IS_USER_ONBOARDED } from "../../constants/UserDefaults";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 
 type Props = {
     navigation: any
@@ -26,8 +28,13 @@ type Props = {
 
 const OnBoardingRegistration = ({ navigation }: Props) => 
 {
+    const [enabled, setScrollEnabled] = useState(false);
 
-    const [value, onChangeText] = React.useState("Useless Placeholder");
+    useEffect(() => 
+    {
+        const keyboardWillShow = Keyboard.addListener("keyboardWillShow", () => setScrollEnabled(true));
+        const keyboardDidHide = Keyboard.addListener("keyboardDidHide", () => setScrollEnabled(false));
+    });
 
     const register = async () =>
     {
@@ -36,42 +43,49 @@ const OnBoardingRegistration = ({ navigation }: Props) =>
     }
 
     return (
-        <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
-            <TextInput
-                style={styles.input}
-                placeholder="Username"
-                autoCapitalize="none"
-                placeholderTextColor="white"
-            // onChangeText={val => this.onChangeText("username", val)}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Password"
-                secureTextEntry={true}
-                autoCapitalize="none"
-                placeholderTextColor="white"
-            // onChangeText={val => this.onChangeText("password", val)}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Email"
-                autoCapitalize="none"
-                placeholderTextColor="white"
-            // onChangeText={val => this.onChangeText("email", val)}
-            />
-            <TextInput
-                style={styles.input}
-                placeholder="Phone Number"
-                autoCapitalize="none"
-                placeholderTextColor="white"
-            // onChangeText={val => this.onChangeText("phone_number", val)}
-            />
-            <Button
-                style={styles.button}
-                title="Sign Up"
-            // onPress={this.signUp}
-            />
-        </KeyboardAvoidingView>
+        <KeyboardAwareScrollView
+            contentContainerStyle={{ justifyContent: 'center', alignItems: 'center', height: "100%" }}
+            scrollEnabled={enabled}
+            extraScrollHeight={10}>
+            <View style={styles.container}>
+                <TextInput
+                    style={styles.input}
+                    placeholder="Username"
+                    autoCapitalize="none"
+                    placeholderTextColor="white"
+                // onChangeText={val => this.onChangeText("username", val)}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Password"
+                    secureTextEntry={true}
+                    autoCapitalize="none"
+                    placeholderTextColor="white"
+                // onChangeText={val => this.onChangeText("password", val)}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Email"
+                    autoCapitalize="none"
+                    placeholderTextColor="white"
+                // onChangeText={val => this.onChangeText("email", val)}
+                />
+                <TextInput
+                    style={styles.input}
+                    placeholder="Phone Number"
+                    autoCapitalize="none"
+                    placeholderTextColor="white"
+                // onChangeText={val => this.onChangeText("phone_number", val)}
+                />
+                <Button
+                    style={styles.button}
+                    title="Sign Up"
+                // onPress={this.signUp}
+                />
+            </View>
+        </KeyboardAwareScrollView>
+        // <KeyboardAvoidingView style={styles.container} behavior="padding" enabled>
+        // </KeyboardAvoidingView>
         // <KeyboardAvoidingView style={styles.container}>
         //     <TextInput
         //         style={{ height: 40, borderColor: "gray", borderWidth: 1 }}
