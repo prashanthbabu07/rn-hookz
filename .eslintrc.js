f2daeb9efa93f7aa32c1af44a6620939f57d801f
Prashanth Babu T <prashanthbabu07@gmail.com>
2019-09-18 19:05:37 +0530

    fixed eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,7 +1,7 @@
 module.exports = {
-    root: true,
-    extends: "@react-native-community",
-    rules: [
-        quotes: ["error", "double"]
+    "root": true,
+    "extends": "@react-native-community",
+    "rules": [
+        "quotes": ["error", "double"]
     ]
 };
