c6e9852bb43f671c44379314ed981ecbb4319153
Prashanth Babu T <prashanthbabu07@gmail.com>
2020-10-23 21:45:45 +0530

    Update reducer.js

--- src/features/Counter/reducer.js
+++ src/features/Counter/reducer.js
@@ -2,24 +2,24 @@ import { AX_INCREMENT_COUNTER, AX_DECREMENT_COUNTER } from './constants';
 
 
 const initialState = {
-    counter: 0
+	counter: 0
 }
 
 const counterReducer = (state = initialState, action) =>
 {
-    switch (action.type)
-    {
-        case AX_INCREMENT_COUNTER:
-            return {
-                counter: state.counter + 1
-            }
-        case AX_DECREMENT_COUNTER:
-            return {
-                counter: state.counter - 1
-            }
-        default:
-            return state;
-    }
+	switch (action.type)
+	{
+		case AX_INCREMENT_COUNTER:
+			return {
+				counter: state.counter + 1
+			}
+		case AX_DECREMENT_COUNTER:
+			return {
+				counter: state.counter - 1
+			}
+		default:
+			return state;
+	}
 }
 
 export default counterReducer;
